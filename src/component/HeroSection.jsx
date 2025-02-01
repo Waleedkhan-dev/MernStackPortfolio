ea1991f57b290f5098b877eb78d9a9c0d54305d2
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-02-01 01:47:02 -0800

    linkedinurl

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -7,49 +7,50 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { Link } from "react-router-dom"
 
 const HeroSection = () => {
- return (
-  <Fragment>
-   <div>
-    <div className="bg-[#090003] ">
-     <div className="flex justify-center items-center gap-12">
-      <div className="w-[43%]">
-       <h1 className="text-white text-4xl font-semibold">Hi, I’m <span className="text-[#FF014F]">waleed</span>  <br /> <span>a</span>
-        <span className="text-white gap-2">
-
-         <Typewriter
-          words={["Full Stack Developer", "MERN Stack Developer", "DevOps Engineer"]}
-          loop={0}
-          cursor
-          cursorStyle='|'
-          typeSpeed={50}
-          deleteSpeed={50}
-          delaySpeed={500}
-         >
-
-         </Typewriter>
-        </span>
-       </h1>
-       <div className="mt-2">
-        <p className="text-white">
-         I am a MERN Stack Developer specializing in responsive web apps with React, Node.js, Express, and MongoDB. I build modern frontends, scalable backends, and seamless APIs for a smooth user experience. Let’s create something amazing! </p>
-       </div>
-       <div>
-        <h1>find with me</h1>
-        <Link className="flex justify-start items-center gap-10">
-         <Link className="text-white flex  justify-center bg-[#1E2125] h-11 w-11 items-center text-center"><FaFacebookF /></Link>
-         <p className="text-white flex  justify-center bg-[#1E2125] h-11 w-11 items-center text-center"><FaLinkedinIn /></p>
-         <Link className="text-white flex  justify-center bg-[#1E2125] h-11 w-11 items-center text-center"><FaInstagram /></Link>
-        </Link>
-       </div>
-      </div>
-
+  const openLinkedin = () => { window.open("https://www.linkedin.com/in/waleed-ahmed-9b4b2a1b0/") };
+  return (
+    <Fragment>
       <div>
-       <img src={pic1} alt="" />
+        <div className="bg-[#090003] ">
+          <div className="flex justify-center items-center gap-12">
+            <div className="w-[43%]">
+              <h1 className="text-white text-4xl font-semibold">Hi, I’m <span className="text-[#FF014F]">waleed</span>  <br /> <span>a</span>
+                <span className="text-white gap-2">
+
+                  <Typewriter
+                    words={["Full Stack Developer", "MERN Stack Developer", "DevOps Engineer"]}
+                    loop={0}
+                    cursor
+                    cursorStyle='|'
+                    typeSpeed={50}
+                    deleteSpeed={50}
+                    delaySpeed={500}
+                  >
+
+                  </Typewriter>
+                </span>
+              </h1>
+              <div className="mt-2">
+                <p className="text-white">
+                  I am a MERN Stack Developer specializing in responsive web apps with React, Node.js, Express, and MongoDB. I build modern frontends, scalable backends, and seamless APIs for a smooth user experience. Let’s create something amazing! </p>
+              </div>
+              <div>
+                <h1>find with me</h1>
+                <div className="flex justify-start items-center gap-10">
+                  <button className="text-white flex  justify-center bg-[#1E2125] h-11 w-11 items-center text-center"><FaFacebookF /></button>
+                  <button onClick={openLinkedin} className="text-white flex  justify-center bg-[#1E2125] h-8 w-8  items-center text-center"><FaLinkedinIn className="w-full h-full" /></button>
+                  <button className="text-white flex  justify-center bg-[#1E2125] h-11 w-11 items-center text-center"><FaInstagram /></button>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <img src={pic1} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
-     </div>
-    </div>
-   </div>
-  </Fragment >
- )
+    </Fragment >
+  )
 }
 export default HeroSection
